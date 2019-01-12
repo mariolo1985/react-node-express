0a5e74779015e16863a3b1bfcbb2b8676f77e499
mariolo1985 <loserpunkx182@gmail.com>
2019-01-11 23:45:39 -0700

    hydrate instead of render

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from '../components/App';
 
 (() => {
-    ReactDOM.render(<App />, document.getElementById('app'));
+    ReactDOM.hydrate(<App />, document.getElementById('app'));
 })();
