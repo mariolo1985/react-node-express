5fbdab7213424515b4dc92059dcfdd0098d5ce47
Mario Lo <kinfonglo@overstock.com>
2019-01-15 18:04:39 -0700

    added put services

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
     constructor(props) {
@@ -14,7 +15,19 @@ class App extends Component {
         this.setState({
             isExpanded: !isExpanded
         });
+
+        try {
+            this.putDataToDB('My test db message');
+        } catch (e) {
+            console.log('putDataToDB err: ', e);
+        }
     }
+
+    putDataToDB = (message) => {
+        axios.post('/api/putData', {
+            message
+        });
+    };
 
     render() {
         const { isExpanded } = this.state;
