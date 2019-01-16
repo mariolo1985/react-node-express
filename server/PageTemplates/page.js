023e60ac6e44e2007d03243a4ded37128e9532b5
Mario Lo <kinfonglo@overstock.com>
2019-01-15 19:46:09 -0700

    updated markup

--- server/PageTemplates/page.js
+++ server/PageTemplates/page.js
@@ -3,13 +3,12 @@ export const getDefaultPageTemplate = (reactDom) => {
     <html lang="en">
     <head>
       <meta charset="utf-8">
-      <title>Test</title>
+      <title>Hello! It's a me!</title>
       <link href="css/master.min.css" rel="stylesheet">
     </head>
     <body>
       <div class="content">
          <div id="app" class="wrap-inner">
-            <!--- magic happens here --> 
             ${reactDom}
          </div>
       </div>
